555922f4c08334e801dceed421fa20285212f7fa
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-06-15 13:10:52 -0300

    buttons of music added in player

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Slider from 'rc-slider';
 
-import { Container, Current, Volume } from './styles';
+import { Container, Current, Volume, Progress, Controls } from './styles';
 
 import VolumeIcon from '../../assets/images/volume.svg';
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
 
 const Player = () => (
   <Container>
@@ -15,6 +21,16 @@ const Player = () => (
         <small>Arthist</small>
       </div>
     </Current>
+
+    <Progress>
+      <Controls>
+        <button><img src={ShuffleIcon} alt="Shuffle" /></button>
+        <button><img src={BackwardIcon} alt="Backward" /></button>
+        <button><img src={PlayIcon} alt="Play" /></button>
+        <button><img src={ForwardIcon} alt="Forward" /></button>
+        <button><img src={RepeatIcon} alt="Repeat" /></button>
+      </Controls>
+    </Progress>
 
     <Volume>
       <img src={VolumeIcon} alt="volumn" />
