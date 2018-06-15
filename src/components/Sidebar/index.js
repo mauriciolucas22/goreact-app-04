edaee4a091a98ad5208fbdeb1547df3c567718ad
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-06-15 12:22:12 -0300

    sidebar ok

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,12 +7,53 @@ import addPlayListIcon from '../../assets/images/add_playlist.svg';
 const Sidebar = () => (
   <Container>
     <div>
-      <Nav>
+      <Nav main>
         <li>
           <a href="">Navegar</a>
         </li>
         <li>
           <a href="">Rádio</a>
+        </li>
+      </Nav>
+      <Nav>
+        <li>
+          <span>Sua Biblioteca</span>
+        </li>
+        <li>
+          <a href="">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="">Tocados recentemente</a>
+        </li>
+        <li>
+          <a href="">Músicas</a>
+        </li>
+        <li>
+          <a href="">Álbums</a>
+        </li>
+        <li>
+          <a href="">Artistas</a>
+        </li>
+        <li>
+          <a href="">Estações</a>
+        </li>
+        <li>
+          <a href="">Arquivos locais</a>
+        </li>
+        <li>
+          <a href="">Vídeos</a>
+        </li>
+        <li>
+          <a href="">Podcasts</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>Playlists</span>
+        </li>
+        <li>
+          <a href="">Melhores do ...</a>
         </li>
       </Nav>
     </div>
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+// > primeira div
 export const Container = styled.aside`
   height: 100%;
   width: 200px;
@@ -9,21 +10,39 @@ export const Container = styled.aside`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  > div {
+    padding: 25px;
+  }
 `;
 
 export const Nav = styled.ul`
   list-style: none;
+  margin-top: 25px;
+
+  &:first-child {
+    margin: 0;
+  }
 
   li {
     a {
       color: inherit;
       text-decoration: none;
       font-size: 13px;
-      font-weight: 32px;
+      line-height: 32px;
+      font-weight: ${props => (props.main ? 'bold' : 'normal')};
 
       &:hover {
         color: #fff;
       }
+    }
+
+    span {
+      font-size: 11px;
+      text-transform: uppercase;
+      line-height: 22px;
+      letter-spacing: 1.11px;
+      font-weight: 300;
     }
   }
 `;
