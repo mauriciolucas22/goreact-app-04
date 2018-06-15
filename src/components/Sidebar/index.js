27b0b8696bb99f391bae12008d83e928f39d516b
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-06-15 12:12:02 -0300

    button newPlayList added

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, NewPlaylist, Nav } from './styles';
+
+import addPlayListIcon from '../../assets/images/add_playlist.svg';
 
 const Sidebar = () => (
-  <Container />
+  <Container>
+    <div>
+      <Nav>
+        <li>
+          <a href="">Navegar</a>
+        </li>
+        <li>
+          <a href="">Rádio</a>
+        </li>
+      </Nav>
+    </div>
+    <NewPlaylist>
+      <img src={addPlayListIcon} alt="addPlayList" />
+      Nova Playlist
+    </NewPlaylist>
+  </Container>
 );
 
 export default Sidebar;
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -10,3 +10,39 @@ export const Container = styled.aside`
   flex-direction: column;
   justify-content: space-between;
 `;
+
+export const Nav = styled.ul`
+  list-style: none;
+
+  li {
+    a {
+      color: inherit;
+      text-decoration: none;
+      font-size: 13px;
+      font-weight: 32px;
+
+      &:hover {
+        color: #fff;
+      }
+    }
+  }
+`;
+
+export const NewPlaylist = styled.button`
+  background: transparent;
+  border: 0;
+  border-top: 1px solid #282828;
+  font-size: 13px;
+  color: #b3b3b3;
+  display: flex;
+  align-items: center;
+  padding: 15px 25px;
+
+  &:hover {
+    color: #fff;
+  }
+
+  img {
+    margin-right: 10px;
+  }
+`;
