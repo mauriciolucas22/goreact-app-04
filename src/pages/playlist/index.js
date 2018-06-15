85105224ca3e0ba2d13f880ace08089da496e15e
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-06-15 18:54:46 -0300

    header of playlist ok

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Header } from './styles';
 
 const Playlist = () => (
-  <Container><h1>Playlist</h1></Container>
+  <Container>
+    <Header>
+      <img src="https://www.enorxistrosi.gr/wp-content/uploads/2017/05/vinyl-disc-wallpaper-5589.jpg" alt="Playlist" />
+
+      <div>
+        <span>Playlist</span>
+        <h1>Lista 1</h1>
+        <p>10 músicas</p>
+
+        <button>Play</button>
+      </div>
+    </Header>
+  </Container>
 );
 
 export default Playlist;
