135d06a4d6fdadbb969031200bc23c6be5a01649
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-06-27 22:54:26 -0300

    playlist connected with saga

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -5,6 +5,8 @@ import { bindActionCreators } from 'redux';
 
 import { Actions as playlistDetailsActions } from '../../store/ducks/playlistDetails';
 
+import Loading from '../../components/Loading';
+
 import { Container, Header, SongList } from './styles';
 
 import ClockIcon from '../../assets/images/clock.svg';
@@ -25,63 +27,49 @@ class Playlist extends Component {
     this.props.getPlaylistDetailsRequest(id);
   };
 
+  renderDetails = () => (
+    <Container>
+      <Header>
+        <img src="https://www.enorxistrosi.gr/wp-content/uploads/2017/05/vinyl-disc-wallpaper-5589.jpg" alt="Playlist" />
+
+        <div>
+          <span>Playlist</span>
+          <h1>title</h1>
+          <p>10 músicas</p>
+
+          <button>Play</button>
+        </div>
+      </Header>
+
+      <SongList cellPadding={0} cellSpacing={0}>
+        <thead>
+          <th />
+          <th>Título</th>
+          <th>Artista</th>
+          <th>Album</th>
+          <th><img src={ClockIcon} alt="clock" /></th>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td><img src={PlusIcon} alt="plus" /></td>
+            <td>Nome da música</td>
+            <td>Nome do album</td>
+            <td>Nome playlist</td>
+            <td>3:36</td>
+          </tr>
+        </tbody>
+      </SongList>
+    </Container>
+  );
+
   render() {
-    return (
-      <Container>
-        <Header>
-          <img src="https://www.enorxistrosi.gr/wp-content/uploads/2017/05/vinyl-disc-wallpaper-5589.jpg" alt="Playlist" />
-
-          <div>
-            <span>Playlist</span>
-            <h1>Lista 1</h1>
-            <p>10 músicas</p>
-
-            <button>Play</button>
-          </div>
-        </Header>
-
-        <SongList cellPadding={0} cellSpacing={0}>
-          <thead>
-            <th />
-            <th>Título</th>
-            <th>Artista</th>
-            <th>Album</th>
-            <th><img src={ClockIcon} alt="clock" /></th>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td><img src={PlusIcon} alt="plus" /></td>
-              <td>Nome da música</td>
-              <td>Nome do album</td>
-              <td>Nome playlist</td>
-              <td>3:36</td>
-            </tr>
-            <tr>
-              <td><img src={PlusIcon} alt="plus" /></td>
-              <td>Nome da música</td>
-              <td>Nome do album</td>
-              <td>Nome playlist</td>
-              <td>3:36</td>
-            </tr>
-            <tr>
-              <td><img src={PlusIcon} alt="plus" /></td>
-              <td>Nome da música</td>
-              <td>Nome do album</td>
-              <td>Nome playlist</td>
-              <td>3:36</td>
-            </tr>
-            <tr>
-              <td><img src={PlusIcon} alt="plus" /></td>
-              <td>Nome da música</td>
-              <td>Nome do album</td>
-              <td>Nome playlist</td>
-              <td>3:36</td>
-            </tr>
-          </tbody>
-        </SongList>
-      </Container>
-    );
+    return this.props.playlistDetails.loading ?
+      (
+        <Container loading>
+          <Loading />
+        </Container>
+      ) : this.renderDetails();
   }
 }
 
