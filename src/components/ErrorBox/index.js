51eee5bcdf7073630c280d6a6886998dc4643851
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-06-29 20:08:04 -0300

    ErrorBox connected with redux

--- src/components/ErrorBox/index.js
+++ src/components/ErrorBox/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Actions as ErrorActions } from '../../store/ducks/error';
+
 import { Container } from './styles';
 
 import CloseIcon from '../../assets/images/close.svg';
@@ -13,4 +17,11 @@ const ErrorBox = () => (
   </Container>
 );
 
-export default ErrorBox;
+const mapStateToProps = state => ({
+  error: state.error,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(ErrorActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ErrorBox);
